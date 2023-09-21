feba055b26dd69c7df745dfc3aa66a8395462781
soledardes <soledardes@gmail.com>
2023-09-21 19:58:19 +0100

    added functionality of app

--- javascript/index.js
+++ javascript/index.js
@@ -1,26 +1,52 @@
 function updateTime() {
   //Lisbon
   let lisbonElement = document.querySelector("#lisbon");
-  let lisbonDateElement = lisbonElement.querySelector(".date");
-  let lisbonTimeElement = lisbonElement.querySelector(".time");
-  let lisbonTime = moment().tz("Europe/Lisbon");
+  if (lisbonElement) {
+    let lisbonDateElement = lisbonElement.querySelector(".date");
+    let lisbonTimeElement = lisbonElement.querySelector(".time");
+    let lisbonTime = moment().tz("Europe/Lisbon");
 
-  lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
-  lisbonTimeElement.innerHTML = lisbonTime.format(
-    "H:mm:ss [<small>]A[</small>]"
-  );
+    lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
+    lisbonTimeElement.innerHTML = lisbonTime.format(
+      "H:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   //Buenos Aires
   let buenosAiresElement = document.querySelector("#buenos-aires");
-  let buenosAiresDateElement = buenosAiresElement.querySelector(".date");
-  let buenosAiresTimeElement = buenosAiresElement.querySelector(".time");
-  let buenosAiresTime = moment().tz("America/Argentina/Buenos_Aires");
+  if (buenosAiresElement) {
+    let buenosAiresDateElement = buenosAiresElement.querySelector(".date");
+    let buenosAiresTimeElement = buenosAiresElement.querySelector(".time");
+    let buenosAiresTime = moment().tz("America/Argentina/Buenos_Aires");
 
-  buenosAiresDateElement.innerHTML = buenosAiresTime.format("MMMM Do YYYY");
-  buenosAiresTimeElement.innerHTML = buenosAiresTime.format(
-    "H:mm:ss [<small>]A[</small>]"
-  );
+    buenosAiresDateElement.innerHTML = buenosAiresTime.format("MMMM Do YYYY");
+    buenosAiresTimeElement.innerHTML = buenosAiresTime.format(
+      "H:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("H:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small> </div>
+    </div>
+  `;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
