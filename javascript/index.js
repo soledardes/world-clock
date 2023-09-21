e8c9f58188e5b7b85e557b16e5bcb2f24c733af7
soledardes <soledardes@gmail.com>
2023-09-21 19:14:23 +0100

    added time and date for selected cities

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,26 @@
+function updateTime() {
+  //Lisbon
+  let lisbonElement = document.querySelector("#lisbon");
+  let lisbonDateElement = lisbonElement.querySelector(".date");
+  let lisbonTimeElement = lisbonElement.querySelector(".time");
+  let lisbonTime = moment().tz("Europe/Lisbon");
 
+  lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
+  lisbonTimeElement.innerHTML = lisbonTime.format(
+    "H:mm:ss [<small>]A[</small>]"
+  );
+
+  //Buenos Aires
+  let buenosAiresElement = document.querySelector("#buenos-aires");
+  let buenosAiresDateElement = buenosAiresElement.querySelector(".date");
+  let buenosAiresTimeElement = buenosAiresElement.querySelector(".time");
+  let buenosAiresTime = moment().tz("America/Argentina/Buenos_Aires");
+
+  buenosAiresDateElement.innerHTML = buenosAiresTime.format("MMMM Do YYYY");
+  buenosAiresTimeElement.innerHTML = buenosAiresTime.format(
+    "H:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
